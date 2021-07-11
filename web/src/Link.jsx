41f8df784134680422f99006f8edd9b03b433985
Enrique Alpízar <ealpizr@gmail.com>
2021-07-10 23:48:22 -0600

    create link component

---
+++ web/src/Link.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Link.css'
+
+const Link = url => {
+  return (
+    <div className="link-container">
+      <p className="link-p"><a href={url}>{url}</a></p>
+      <button className="link-copy-button">copy</button>
+    </div>
+  )
+}
+
+export default Link
--- web/src/Main.jsx
+++ web/src/Main.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import Link from './Link'
 import './Main.css'
 
 const Main = () => {
+
+  const inputRef = useRef("")
+  const [err, setErr] = useState("")
+  const [url, setUrl] = useState("")
+
+  const shortenLink = async e => {
+    e.preventDefault()
+    const link = inputRef.current.value
+    if (!link) {
+      setErr("You need to enter a link")
+    }
+
+    fetch('http://localhost:3001/api/shorten', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ url: link })
+    }).then(res => res.json()).then(data => {
+      setUrl(data.shortenedUrl)
+    }).catch(e => setErr(e.message))
+  }
+
   return (
     <main className="main-container">
       <form className="form" action="">
-      <input className="form-input" type="text" placeholder="Long url..."/>
-      <input className="form-button" type="submit" value="Shorten It!" />
+      {err && <p>{err} </p>}
+      <input ref={inputRef} onChange={() => {setErr("")}} className="form-input" type="text" placeholder="Long url..."/>
+      {url && <Link url={url} />}
+      <input className="form-button" type="submit" value="Shorten It!" onClick={shortenLink} />
     </form>
     </main>
   )
