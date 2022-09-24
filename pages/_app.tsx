a7a7ba165c4c104f2dd5e7ea2cc2d83357bcfe2a
Enrique Alpízar <ealpizr@gmail.com>
2022-09-23 18:40:33 -0700

    chore: refactor project structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
+import "@styles/reset.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import "styles/reset.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
