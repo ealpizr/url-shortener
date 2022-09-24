deb1d2831ec45a03a87693264f0fabdd2e2f07f0
Enrique Alpízar <ealpizr@gmail.com>
2022-09-24 00:00:28 -0700

    feat: add a basic ui (not functional)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
+import styles from "@styles/index.module.scss";
+import { AiFillGithub } from "react-icons/ai";
+import { FaCut } from "react-icons/fa";
+
 const Home = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className={styles.container}>
+      <nav className={styles.header}>
+        <h2 className={styles.title}>URL Shortener</h2>
+        <a
+          href="https://github.com/ealpizr/url-shortener-2"
+          className={styles["github-link"]}
+        >
+          <AiFillGithub />
+        </a>
+      </nav>
+      <section className={styles.main}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Paste your long url here"
+        />
+        <button className={styles.button}>
+          <FaCut />
+          Shorten it
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
