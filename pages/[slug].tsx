5f9b43daef4b0646f9cf1d2cf98bc962c0d21eca
Enrique Alpízar <ealpizr@gmail.com>
2022-09-23 23:16:09 -0700

    feat: add slug redirect

---
+++ pages/[slug].tsx
@@ -0,0 +1,50 @@
+import { prisma } from "@lib/prisma";
+import { NotFoundError } from "@prisma/client/runtime";
+import { GetServerSideProps } from "next";
+import { NextParsedUrlQuery } from "next/dist/server/request-meta";
+import Error from "next/error";
+
+type StatusCode = 404 | 500;
+
+interface Props {
+  statusCode: StatusCode;
+}
+
+interface Params extends NextParsedUrlQuery {
+  slug: string;
+}
+
+const SlugPage = ({ statusCode }: Props) => {
+  return <Error statusCode={statusCode} />;
+};
+
+const getServerSideProps: GetServerSideProps<Props, Params> = async (
+  context
+) => {
+  try {
+    const slug = context.params!.slug;
+    const record = await prisma.uRL.findFirstOrThrow({
+      where: { slug },
+    });
+    return {
+      redirect: {
+        destination: record.url,
+        permanent: true,
+      },
+    };
+  } catch (e) {
+    let statusCode: StatusCode = 404;
+    if (e instanceof NotFoundError === false) {
+      statusCode = 500;
+      console.error(e);
+    }
+    return {
+      props: {
+        statusCode,
+      },
+    };
+  }
+};
+
+export default SlugPage;
+export { getServerSideProps };
