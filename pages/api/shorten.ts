cc30403c5705431093fd2074a0c4d76632a27fb4
Enrique Alpízar <ealpizr@gmail.com>
2022-09-27 16:58:14 -0700

    chore: check if url already exists before inserting to db

--- pages/api/shorten.ts
+++ pages/api/shorten.ts
@@ -16,22 +16,25 @@ const handler = async (
     });
   }
 
-  const slug = cryptoRandomString({ length: 6, type: "distinguishable" });
-  await prisma.uRL
-    .create({
-      data: {
-        url: body.data.url,
-        slug: slug,
-      },
-    })
-    .catch((e) => {
-      console.error(e);
-      return res.status(500).json({
-        error: "internal server error",
-        message: "could not create shortened url",
+  let slug = (await prisma.uRL.findFirst({ where: { url: body.data.url } }))
+    ?.slug;
+  if (slug === undefined) {
+    slug = cryptoRandomString({ length: 6, type: "distinguishable" });
+    await prisma.uRL
+      .create({
+        data: {
+          url: body.data.url,
+          slug: slug,
+        },
+      })
+      .catch((e) => {
+        console.error(e);
+        return res.status(500).json({
+          error: "internal server error",
+          message: "could not create shortened url",
+        });
       });
-    });
-
+  }
   res.status(201).json({ message: "url shortened successfully", slug: slug });
 };
 
